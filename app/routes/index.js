567ff806c26bdb8b101f84b8d08c92ab740ae5d4
lalawow <lalawow@gmail.com>
2016-05-02 14:35:09 +0800

    for project API Basejump: Timestamp microservice

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
+
 module.exports = function (app, passport) {
 
 	function isLoggedIn (req, res, next) {
@@ -15,11 +16,40 @@ module.exports = function (app, passport) {
 
 	var clickHandler = new ClickHandler();
 
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
+	app.route('/').get(function (req, res) {
+
+			console.log("hello there!")
 		});
 
+		app.route('/:_id').get(function (req, res) {
+            var id = req.params._id
+            var timeIndex
+            var answer = {}
+			console.log(id+"ok then!")
+			if (filterInt(id)>0) {
+				timeIndex = filterInt(id)*1000
+
+			} else {
+				timeIndex = Date.parse(id)                       
+			}
+   
+			if (timeIndex==NaN) {
+				answer = {"unix":null,"natural":null}
+				} else {
+					var options = { year: 'numeric', month: 'long', day: 'numeric' };
+					var time = new Date()
+					time.setTime(timeIndex)
+					answer = {"unix":time.getTime()/1000,"natural":time.toLocaleDateString('en-US', options)}
+				}
+			
+            console.log(answer.unix+"  "+answer.natural)
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(answer))
+
+		});
+
+
+/*
 	app.route('/login')
 		.get(function (req, res) {
 			res.sendFile(path + '/public/login.html');
@@ -54,4 +84,12 @@ module.exports = function (app, passport) {
 		.get(isLoggedIn, clickHandler.getClicks)
 		.post(isLoggedIn, clickHandler.addClick)
 		.delete(isLoggedIn, clickHandler.resetClicks);
+
+		*/
 };
+
+var filterInt = function (value) {
+  if(/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
+    return Number(value);
+  return NaN;
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 
@@ -10,7 +10,7 @@ var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+//mongoose.connect(process.env.MONGO_URI);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 routes(app, passport);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 7000;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
